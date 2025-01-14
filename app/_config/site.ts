c3dfb567b6b44a55db16e78539f830d5c1c91570
Anthony Davi <ant.davi07@gmail.com>
2025-01-14 14:28:32 -0300

    chore: initial prisma setup

--- app/_config/site.ts
+++ app/_config/site.ts
@@ -1,5 +1,5 @@
 export const NavbarNavigation = [
     { id: '1', label: "Dashboard", href: "/" },
-    { id: '2', label: "Transações", href: "/trasacoes" },
+    { id: '2', label: "Transações", href: "/transacoes" },
     { id: '3', label: "Gerencia", href: "/gerencia" },
 ]
